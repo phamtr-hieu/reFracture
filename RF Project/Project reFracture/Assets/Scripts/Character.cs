8c700038a9e595a876dd61ce3d13e6df62c37273
hieupham154 <phamtrhieu154@gmail.com>
2022-05-12 02:07:20 +0700

    Added Input System and movement (sort of)

--- RF Project/Project reFracture/Assets/Scripts/Character.cs
+++ RF Project/Project reFracture/Assets/Scripts/Character.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Character : MonoBehaviour
 {
+    public PlayerInput playerInput;
+    private Rigidbody2D rb;
+    [SerializeField] private Vector2 moveInput;
+    [SerializeField] private float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,4 +21,18 @@ public class NewBehaviourScript : MonoBehaviour
     {
         
     }
+
+    void FixedUpdate()
+    {
+        if(moveInput.x !=0)
+        {
+            rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y );
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+	{
+		moveInput = context.ReadValue<Vector2>();
+        Debug.Log(rb.velocity);
+	}
 }
