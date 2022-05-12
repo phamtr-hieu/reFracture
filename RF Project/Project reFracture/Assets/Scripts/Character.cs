ee33d9d3a6dca43f245320bbc5df7b1e980ca1ee
thebenjiechang <thebenjiechang@gmail.com>
2022-05-13 00:20:05 +0800

    Added player attacks and character flipping

--- RF Project/Project reFracture/Assets/Scripts/Character.cs
+++ RF Project/Project reFracture/Assets/Scripts/Character.cs
@@ -7,6 +7,13 @@ public class Character : MonoBehaviour
 {
     //public PlayerInput playerInput;
     private Rigidbody2D rb;
+    private Animator anim;
+
+    #region Attack Vars
+    int attackID = 1;
+    [SerializeField] float attackTimer = 1.5f;
+    float lastAttackTime;
+    #endregion
 
     #region Vectors
     [SerializeField] private Vector2 moveInput;
@@ -19,25 +26,36 @@ public class Character : MonoBehaviour
 
     #region Bools
     [SerializeField] private bool _jump;
+    private bool _facingRight = true;
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
-    {  
+    {
         #region Move
-        transform.position += new Vector3(moveInput.x,0,0) * Time.deltaTime * moveSpeed;
+        transform.position += new Vector3(moveInput.x, 0, 0) * Time.deltaTime * moveSpeed;
         #endregion
 
         #region Jump
-        if(_jump)
+        if (_jump)
         {
-            rb.velocity = Vector2.up *jumpForce;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        #endregion
+
+        #region Attack
+        lastAttackTime += Time.deltaTime;
+        if (lastAttackTime > attackTimer)
+        {
+            attackID = 1;
+            lastAttackTime = 0;
         }
         #endregion
     }
@@ -53,21 +71,47 @@ public class Character : MonoBehaviour
     }
 
     public void OnMove(InputAction.CallbackContext context)
-	{
-		moveInput = context.ReadValue<Vector2>();
-	}
+    {
+        moveInput = context.ReadValue<Vector2>();
+        if (moveInput.x > 0 && !_facingRight)
+        {
+            flip();
+            _facingRight = true;
+        }
+        else if (moveInput.x < 0 && _facingRight)
+        {
+            flip();
+            _facingRight = false;
+        }
+    }
 
     public void OnJump(InputAction.CallbackContext context)
-	{
-        if(context.started)
+    {
+        if (context.started)
         {
             _jump = true;
-        } 
-        else if(context.canceled)
+        }
+        else if (context.canceled)
         {
             _jump = false;
         }
+    }
 
-		
-	}
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            anim.SetTrigger("attack" + attackID);
+            print(attackID);
+            lastAttackTime = 0;
+            attackID++;
+            if (attackID > 3)
+                attackID = 1;
+        }
+    }
+
+    void flip()
+    {
+        this.transform.Rotate(0, 180, 0);
+    }
 }
