345ac0d204c482ba8446e8fff54049d47aff0bcc
hieupham154 <phamtrhieu154@gmail.com>
2022-05-12 12:51:32 +0700

    Added jump
    Added a very wonky jump, to be improve

--- RF Project/Project reFracture/Assets/Scripts/Character.cs
+++ RF Project/Project reFracture/Assets/Scripts/Character.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    public PlayerInput playerInput;
+    //public PlayerInput playerInput;
     private Rigidbody2D rb;
+
+    #region Vectors
     [SerializeField] private Vector2 moveInput;
+    #endregion
+
+    #region Floats
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    #endregion
+
+    #region Bools
+    [SerializeField] private bool _jump;
+    #endregion
 
     // Start is called before the first frame update
     void Start()
@@ -18,21 +29,45 @@ public class Character : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {  
+        #region Move
+        transform.position += new Vector3(moveInput.x,0,0) * Time.deltaTime * moveSpeed;
+        #endregion
+
+        #region Jump
+        if(_jump)
+        {
+            rb.velocity = Vector2.up *jumpForce;
+        }
+        #endregion
     }
 
     void FixedUpdate()
     {
+        /*#region Move
         if(moveInput.x !=0)
         {
             rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y );
         }
+        #endregion*/
     }
 
     public void OnMove(InputAction.CallbackContext context)
 	{
 		moveInput = context.ReadValue<Vector2>();
-        Debug.Log(rb.velocity);
+	}
+
+    public void OnJump(InputAction.CallbackContext context)
+	{
+        if(context.started)
+        {
+            _jump = true;
+        } 
+        else if(context.canceled)
+        {
+            _jump = false;
+        }
+
+		
 	}
 }
